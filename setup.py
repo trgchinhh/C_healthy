1cc3c5413f4568c4c0363fbb0ed28dc46fe334f3
Trường Chinh <chinhcuber@gmail.com>
2026-07-06 10:30:57 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import os
 from setuptools import setup, find_packages
 
-# Đọc nội dung README.md
 long_description = ""
 if os.path.exists("README.md"):
     with open("README.md", encoding="utf-8") as f:
@@ -9,7 +8,7 @@ if os.path.exists("README.md"):
 
 setup(
     name="C_healthy",
-    version="0.2.6",
+    version="0.3.0",
     description="A library for calculating body index and and give advice",
     url="https://github.com/trgchinhh/LibraryChealthy",
     author="Nguyen Truong Chinh",
@@ -23,12 +22,40 @@ setup(
         "Operating System :: OS Independent",
     ],
     keywords=[
-              "BMI", "BMR", "TDEE", "INSTRUCT", "WHR", "LBM", "BFP", 
-              "BBW", "IBW", "MA", "FFMI", "RFM", "VFR", "BSA", 
-              "VO2MAX", "HSI", "MMI", "BFM",
-              "NABMI", "NABMR", "NATDEE", "NAWHR", "NALBM", "NABFP", "NABBW", 
-              "NAIBW", "NAMA", "NAFFMI", "NARFM", "NAVFR", "NABSA", "NAVO2MAX",
-              "NAHSI", "NAMMI", "NABFM"
+        "__version__", "INSTRUCT",
+
+        "BMI", "BMR", "TDEE", "WHR", "LBM", "BFP",
+        "BBW", "IBW", "MA", "RFM", "VFR", "BSA",
+        "FFMI", "HSI", "MMI", "BFM", "ABSI", "AVI",
+        "BAI", "CI", "WHtR", "FMI", "FFM", "FMR",
+        "TBW", "ECW", "ICW", "BCM", "SMI", "BEE",
+        "REE", "PAL", "MET", "VO2MAX", "ASMI", "MAMC",
+        "PonderalIndex", "MaxHeartRate", "TargetHeartRate",
+        "HeartRateReserve", "CalorieDeficit", "CalorieSurplus",
+        "MUAC_Assessment", "ProteinNeed", "FatNeed",
+        "CarbNeed", "WaterNeed", "IdealWater",
+        "LeanBodyPercentage", "FatPercentage", "MusclePercentage",
+        "BoneMassEstimate", "BMIPrime", "CorpulenceIndex",
+        "RelativeFatMass_Ext", "BodyRoundnessIndex", "ABodyRoundness",
+        "VisceralAdiposityIndex", "LipidAccumulationProduct",
+        "FatMass", "LeanMass", "GenderTest",
+
+        
+        "NABMI", "NABMR", "NATDEE", "NAWHR", "NALBM", "NABFP",
+        "NABBW", "NAIBW", "NAMA", "NAVFR", "NABSA", "NAMMI",
+        "NABFM", "NAAVI", "NABAI", "NACI", "NAFFM", "NAFMR",
+        "NATBW", "NAECW", "NAICW", "NABCM", "NASMI", "NABEE",
+        "NAREE", "NAPAL", "NAMET", "NAFFMI", "NARFM", "NAABSI",
+        "NAWHtR", "NAFMI", "NAASMI", "NAMAMC", "NAMUAC",
+        "NAMaxHeartRate", "NATargetHeartRate", "NACalorieDeficit",
+        "NACalorieSurplus", "NAProteinNeed", "NAFatNeed",
+        "NACarbNeed", "NAWaterNeed", "NAIdealWater",
+        "NAPonderalIndex", "NAVO2MAX", "NAHeartRateReserve",
+        "NALeanBodyPercentage", "NAFatPercentage", "NAMusclePercentage",
+        "NABoneMassEstimate", "NABMIPrime", "NACorpulenceIndex",
+        "NARelativeFatMass", "NABodyRoundnessIndex", "NAABodyRoundness",
+        "NAVisceralAdiposityIndex", "NALipidAccumulationProduct", "NAFatMass",
+        "NALeanMass", "NAHSI",
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
